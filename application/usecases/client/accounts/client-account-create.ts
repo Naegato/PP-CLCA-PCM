1099d37c350d544fc27c0977cf64ec32810fdf18
MC <charlie.mass.cm@gmail.com>
2025-11-08 13:12:01 +0100

    added iban usecase to account entity & update account creation usecase with iban

--- application/usecases/client/accounts/client-account-create.ts
+++ application/usecases/client/accounts/client-account-create.ts
@@ -4,6 +4,9 @@ import { AccountType } from '@pp-clca-pcm/domain/entities/accounts/type';
 import { User } from '@pp-clca-pcm/domain/entities/user';
 import { AccountRepository } from '../../../repositories/account';
 import { ClientProps } from '@pp-clca-pcm/domain/value-objects/user/client';
+import { Iban } from '@pp-clca-pcm/domain/value-objects/iban';
+import { InvalidIbanError } from '@pp-clca-pcm/domain/errors/invalid-iban-format';
+import { AccountCreateError } from '../../../errors/account-create';
 
 export class ClientAccountCreate {
   public constructor(
@@ -12,11 +15,19 @@ export class ClientAccountCreate {
     
   ) {}
 
-  public async execute(user: User, name: string): Promise<Account> {
-    const account = Account.create(user, this.defaultAccountType, name);
+  public async execute(user: User, name: string): Promise<Account | AccountCreateError> {
+
+    //valeur temp
+    const ibanOrError = Iban.generate('30006', '00001', '12345678901');
+
+    if (ibanOrError instanceof InvalidIbanError) {
+      return new AccountCreateError("Failed to generate IBAN");
+    }
+
+    const account = Account.create(user, this.defaultAccountType, ibanOrError, name);
 
     user.updateClientProps(new ClientProps([
-      ...user.clientProps.accounts,
+      ...user.clientProps?.accounts ?? [],
       account
     ]))
 
