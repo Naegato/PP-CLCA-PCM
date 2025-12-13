a97e204029e45ae8b8261ada89ee22a240e4464a
Pierre Ambroise <74421318+Fan2Shrek@users.noreply.github.com>
2025-12-13 20:48:17 +0100

    PLus jamais de ma vie je fais du ts (#2)

---
+++ infrastructure/tests/adapters/redis.test.ts
@@ -0,0 +1,16 @@
+import { getRedisClient, connectRedis } from '@pp-clca-pcm/adapters/repositories/redis/client';
+import { describe, expect, test } from 'vitest';
+
+const databaseProvider = process.env.DB_PROVIDER;
+const isRedis = databaseProvider === 'redis';
+
+describe.skipIf(!isRedis)('Redis adapter', () => {
+  test('adapter working', async () => {
+	connectRedis();
+
+    const db = getRedisClient();
+	db.flushDb();
+
+	expect(await db.keys('*')).toEqual([]);
+  });
+});
