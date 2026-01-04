8e04ed43ab5cfa13835396e775d8e5d9bbe8c88f
Pierre Ambroise <pierre27.ambroise@gmail.com>
2026-01-04 15:41:16 +0100

    Add express blablabla

---
+++ infrastructure/apps/api/express/src/index.ts
@@ -0,0 +1,76 @@
+import dotenv from "dotenv";
+import path from "path";
+import express from 'express';
+
+import { AccountRepository } from "@pp-clca-pcm/application/repositories/account";
+import { RedisAccountRepository } from '@pp-clca-pcm/adapters/repositories/redis/account/account';
+
+import { AccountTypeRepository } from "@pp-clca-pcm/application/repositories/type";
+import { RedisAccountTypeRepository } from '@pp-clca-pcm/adapters/repositories/redis/account/type';
+
+import { DiscussionRepository } from "@pp-clca-pcm/application/repositories/discussion/discussion";
+import { RedisDiscussionRepository } from '@pp-clca-pcm/adapters/repositories/redis/discussion/discussion';
+
+import { MessageRepository } from "@pp-clca-pcm/application/repositories/discussion/message";
+import { RedisMessageRepository } from '@pp-clca-pcm/adapters/repositories/redis/discussion/message';
+
+import { AdvisorRepository } from "@pp-clca-pcm/application/repositories/advisor";
+import { RedisAdvisorRepository } from '@pp-clca-pcm/adapters/repositories/redis/advisor';
+
+import { RedisLoanRepository } from '@pp-clca-pcm/adapters/repositories/redis/loan';
+import { RedisLoanRequestRepository } from '@pp-clca-pcm/adapters/repositories/redis/request-loan';
+import { RedisTransactionRepository } from '@pp-clca-pcm/adapters/repositories/redis/transaction';
+import { RedisUserRepository } from '@pp-clca-pcm/adapters/repositories/redis/user';
+
+import { connectRedis, getRedisClient } from '@pp-clca-pcm/adapters/repositories/redis/client';
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../../../../.env"),
+});
+
+const app = express()
+const port = 3000
+
+// Init repositories
+
+connectRedis();
+const databaseProvider = process.env.DB_PROVIDER;
+
+let dbConnection: any = null;
+
+let accountRepository: AccountRepository|null = null;
+let accountTypeRepository: AccountTypeRepository|null = null;
+
+let disccussionRepository: DiscussionRepository|null = null;
+let messageRepository: MessageRepository|null = null;
+
+let advisorRepository: AdvisorRepository|null = null;
+let loanRepository: any = null;
+let loanRequestRepository: any = null;
+let transactionRepository: any = null;
+let RedisUserRepository: any = null;
+
+if (databaseProvider === 'postgresql') {
+} else if (databaseProvider === 'redis') {
+	dbConnection = getRedisClient;
+
+	accountRepository = new RedisAccountRepository(dbConnection)
+	accountTypeRepository = new RedisAccountTypeRepository(dbConnection);
+
+	disccussionRepository = new RedisDiscussionRepository(dbConnection);
+	messageRepository = new RedisMessageRepository(dbConnection);
+
+	advisorRepository = new RedisAdvisorRepository(dbConnection);
+	loanRepository = new RedisLoanRepository(dbConnection);
+	loanRequestRepository = new RedisLoanRequestRepository(dbConnection);
+	transactionRepository = new RedisTransactionRepository(dbConnection);
+	userRepository = new RedisUserRepository(dbConnection);
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
